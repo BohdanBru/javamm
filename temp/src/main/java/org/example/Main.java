e05ab6533cdcbf433f63502df19473aa9f0e7e76
BohdanBru <mattewgreenman@gmail.com>
2023-03-23 17:01:39 +0100

    Init project structure

---
+++ temp/src/main/java/org/example/Main.java
@@ -0,0 +1,11 @@
+package org.example;
+
+/**
+ * @author Bohdan Brukhovets
+ * @link https://www.linkedin.com/in/bohdan-brukhovets/
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
